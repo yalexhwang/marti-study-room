59984b7b678f7f6c0c13eb9f34f21e33cd970ce5
yalexhwang <y.alex.hwang@gmail.com>
2016-12-16 22:43:08 -0500

    Background color changed, Review page implemented

--- front/scripts.js
+++ front/scripts.js
@@ -173,6 +173,16 @@ function createPageList(currentList, currentPage, numPerPage) {
 	return arr;
 }
 
+function shuffleArray(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+
 
 
 
