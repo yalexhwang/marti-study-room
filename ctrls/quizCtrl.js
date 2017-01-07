86318e9f3375ae8f47d84a6857be6df38b2c1fdc
yalexhwang <y.alex.hwang@gmail.com>
2017-01-06 23:38:31 -0500

    issue with answer options generation fixed

--- ctrls/quizCtrl.js
+++ ctrls/quizCtrl.js
@@ -5,32 +5,29 @@ martiApp.controller('quizCtrl', function($scope, $rootScope, $location, $cookies
 	$scope.user = $cookies.getObject('user');
 
 	//Default Setting
-	$scope.showNotif = 0;
-	$scope.quizOption1 = 20;
+	$scope.showQuizSetting = 1;
+	$scope.showQuiz = 0;
+	$scope.showResult = 0;
+	$scope.quizOption1 = 5;
 	$scope.quizOption2 = "All";
 	$scope.quizOption3 = 4;
 	$scope.quizOption4 = "No";
-	$scope.quizStarted = 0;
+	var userAnswers = [];
+	userAnswers.length = $scope.quizOption1;
 
 	WordBankService.getFullList()
 	.then(function success(rspns) {
 		$scope.fullList = createFullList(rspns.data.doc, convertPartName, convertLngName);
+		console.log($scope.fullList);
 	}, function fail(rspns) {
 		console.log(rspns);
 	});
 
 	$scope.start = function() {
-		$scope.today = new Date().toString().slice(3, 15) + ", " + new Date().toString().slice(0, 3) + "";
-
 		$scope.quizList = [];
 		var fullList = $scope.fullList;
-		console.log($scope.quizOption1);
-		console.log($scope.quizOption2);
-		console.log($scope.quizOption3);
-		console.log($scope.quizOption4);
-		
+
 		if ($scope.quizOption4 == 1) {
-			console.log('dfadfa');
 			$scope.showNotif = 1;
 			$scope.notifMessage = "Timed feature is currently unavailable."
 			return false;
@@ -38,40 +35,74 @@ martiApp.controller('quizCtrl', function($scope, $rootScope, $location, $cookies
 
 		if ($scope.quizOption2 === "All") {
 			$scope.quizList = shuffleArray(fullList).slice(0, $scope.quizOption1);
+			for (var i = 0; i < $scope.quizList.length; i++) {
+				var options = [];
+				for (var j = 0; j < $scope.fullList.length; j++) {
+					if ($scope.quizList[i].rootIndex !== $scope.fullList[j].rootIndex) {
+						options.push($scope.fullList[j]);
+					}
+				}
+				options = shuffleArray(options).slice(0, $scope.quizOption3 - 1);
+				options.push($scope.quizList[i]);
+				$scope.quizList[i].answerOptions = shuffleArray(shuffleArray(options));
+			}
 		} else {
+			var subList = [];
 			for (var i = 0; i < fullList.length; i++) {
 				if (fullList[i].part == $scope.quizOption2) {
 					$scope.quizList.push(fullList[i]);
+					subList.push(fullList[i]);
 				}
 			}
+			console.log(subList);
 			$scope.quizList = shuffleArray($scope.quizList).slice(0, $scope.quizOption1);
-		}
-
-		//Get an array of possible answer options, including the correct one
-		for (var i = 0; i < $scope.quizList.length; i++) {
-			var options;
-			if (i === 0) {
-				options = shuffleArray($scope.quizList.slice(1));
-			} else if (i === $scope.quizList.length - 1) {
-				options = shuffleArray($scope.quizList.slice(0, $scope.quizList.length - 1));
-			} else {
-				options = shuffleArray($scope.quizList.slice(0, i).concat($scope.quizList.slice(i + 1)));
+			for (var i = 0; i < $scope.quizList.length; i++) {
+				var options = [];
+				for (var j = 0; j < subList.length; j++) {
+					if ($scope.quizList[i].rootIndex !== subList[j].rootIndex) {
+						options.push(subList[j]);
+					}
+				}
+				options = shuffleArray(options).slice(0, $scope.quizOption3 - 1);
+				options.push($scope.quizList[i]);
+				$scope.quizList[i].answerOptions = shuffleArray(shuffleArray(options));
 			}
-			options = options.slice(0, $scope.quizOption3 - 1);
-			options.push($scope.quizList[i]);
-			$scope.quizList[i].answerOptions = shuffleArray(options);
 		}
 		console.log($scope.quizList);
+
 		if ($scope.quizList.length === 0) {
-			$scope.showNotif = 1;
+			$scope.showQuizNotif = 1;
 			$scope.notifMessage = "Sorry, there is no " + convertPartName(Number($scope.quizOption2)); + " in Word Bank."
 		} else {
-			$scope.quizStarted = 1;
+			$scope.showQuiz = 1;
+			$scope.showQuizSetting = 0;
+			$scope.showResult = 0;
+			$scope.today = new Date().toString().slice(3, 15) + ", " + new Date().toString().slice(0, 3) + "";
 		}
 	};
 
+	$scope.select = function(questionIndex, answerIndex) {
+		userAnswers[questionIndex] = answerIndex;
+	};
+	
 	$scope.submit = function() {
-		console.log($scope.selection);
+		console.log(userAnswers);
+		$scope.showResult = 1;
+		$scope.resultIncorrect = [];
+		$scope.resultCorrect = [];
+		for (var i = 0; i < userAnswers.length; i++) {
+			if ($scope.quizList[i].answerOptions[userAnswers[i]].rootIndex === $scope.quizList[i].rootIndex) {
+				$scope.resultCorrect.push($scope.quizList[i]);
+			} else {
+				$scope.quizList[i].questionNo = i + 1;
+				$scope.quizList[i].answerOptions = $scope.quizList[i].answerOptions[userAnswers[i]];
+				$scope.resultIncorrect.push($scope.quizList[i]);
+				$scope.showIncorrectList = 1;
+			}
+		}
+		$scope.score = $scope.resultCorrect.length / $scope.quizList.length * 100;
+		$scope.showResult = 1;
+		console.log($scope.resultIncorrect);
 	};
 
 });
